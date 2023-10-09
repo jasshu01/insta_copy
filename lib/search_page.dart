da57f98fbd5b2f06761b8ce4f92a55fd817cd401
jasshu01 <jashugarg266@gmail.com>
2023-10-09 17:38:15 +0530

    Explore page added- bug fix

--- lib/search_page.dart
+++ lib/search_page.dart
@@ -12,32 +12,27 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.black,
-      child: Column(
-        children: [
-          GridView.builder( itemBuilder: (context,index){
-            return    Image.network(
-              "https://source.unsplash.com/random/400x400?sig=$index",
-              // widget.postList![index].imageUrl,
-              fit: BoxFit.fitWidth,
-              height: 400,
-              width: double.infinity,
-              loadingBuilder: (context, child, loadingProgress) {
-                if (loadingProgress == null) return child;
-                return CircularProgressIndicator();
-              },
-              errorBuilder: (context, error, stackTrace) {
-                return Icon(
-                  Icons.new_releases_outlined,
-                  size: 400,
-                );
-              },
-            );
-          }, gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            crossAxisSpacing: 2,
-            mainAxisSpacing: 2
-          ),),
-        ],
+      child: GridView.builder(
+        itemBuilder: (context, index) {
+          return Image.network(
+            "https://source.unsplash.com/random/400x400?sig=$index",
+            fit: BoxFit.fitWidth,
+            height: 400,
+            width: double.infinity,
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) return child;
+              return CircularProgressIndicator();
+            },
+            errorBuilder: (context, error, stackTrace) {
+              return Icon(
+                Icons.new_releases_outlined,
+                size: 400,
+              );
+            },
+          );
+        },
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3, crossAxisSpacing: 2, mainAxisSpacing: 2),
       ),
     );
   }
